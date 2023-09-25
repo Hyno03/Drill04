92eb104e0a703345a94a836bbb871d30cbbe8f73
Hyno03 <jenny2117@naver.com>
2023-09-25 22:04:56 +0900

    Character move
    code character move (up&down)

--- move_my_character_with_key.py
+++ move_my_character_with_key.py
@@ -6,7 +6,7 @@ tuk_ground = load_image('TUK_GROUND.png')
 character_idle = load_image('character_animation_idle.jpg')
 
 def handle_events():
-    global running, dir_x
+    global running, dir_x, dir_y
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
@@ -16,6 +16,10 @@ def handle_events():
                 dir_x += 1
             elif event.key == SDLK_LEFT:
                 dir_x -= 1
+            elif event.key == SDLK_UP:
+                dir_y += 1
+            elif event.key == SDLK_DOWN:
+                dir_y -= 1
             elif event.key == SDLK_ESCAPE:
                 running = False
         elif event.type == SDL_KEYUP:
@@ -23,11 +27,16 @@ def handle_events():
                 dir_x -= 1
             elif event.key == SDLK_LEFT:
                 dir_x += 1
+            elif event.key == SDLK_UP:
+                dir_y -= 1
+            elif event.key == SDLK_DOWN:
+                dir_y += 1
 
 running = True
-x, y = 800 // 2, 800 //2
+x, y = TUK_WIDTH//2, TUK_HEIGHT//2
 frame = 0
 dir_x = 0
+dir_y = 0
 
 while running:
     clear_canvas()
@@ -37,6 +46,7 @@ while running:
     handle_events()
     frame = (frame + 1) % 6
     x += dir_x * 5
+    y += dir_y * 5
     delay(0.1)
 
 close_canvas()
