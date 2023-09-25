9c6ff29299f04599515dfdd9247505910a4d188a
Hyno03 <jenny2117@naver.com>
2023-09-25 22:10:00 +0900

    Modify character animation
    use character_animation.jpg

--- move_my_character_with_key.py
+++ move_my_character_with_key.py
@@ -3,7 +3,8 @@ from pico2d import*
 TUK_WIDTH, TUK_HEIGHT = 1280, 1024
 open_canvas(TUK_WIDTH, TUK_HEIGHT)
 tuk_ground = load_image('TUK_GROUND.png')
-character_idle = load_image('character_animation_idle.jpg')
+character_idle = load_image('character_animation.jpg')
+#character_idle = load_image('character_animation_idle.jpg')
 
 def handle_events():
     global running, dir_x, dir_y
@@ -41,7 +42,8 @@ dir_y = 0
 while running:
     clear_canvas()
     tuk_ground.draw(TUK_WIDTH//2, TUK_HEIGHT//2)
-    character_idle.clip_draw(frame * 95, 0, 85, 160, x, y)
+    character_idle.clip_draw(frame * 95, 2180, 85, 160, x, y)
+    #character_idle.clip_draw(frame * 95, 0, 85, 160, x, y)
     update_canvas()
     handle_events()
     frame = (frame + 1) % 6
