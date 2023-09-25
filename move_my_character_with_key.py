97b33643e5bba64e578444cfe91c7242fca3d75b
Hyno03 <jenny2117@naver.com>
2023-09-25 21:31:43 +0900

    Input escape & Draw background/ character
    add character idle image
    code input escape and
    draw background and character

--- move_my_character_with_key.py
+++ move_my_character_with_key.py
@@ -3,3 +3,27 @@ from pico2d import*
 TUK_WIDTH, TUK_HEIGHT = 1280, 1024
 open_canvas(TUK_WIDTH, TUK_HEIGHT)
 tuk_ground = load_image('TUK_GROUND.png')
+character_idle = load_image('character_animation_idle.jpg')
+
+def handle_events():
+    global running
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_KEYDOWN:
+            if event.key == SDLK_ESCAPE:
+                running = False
+
+running = True
+x, y = 800 // 2, 800 //2
+
+while running:
+    clear_canvas()
+    tuk_ground.draw(TUK_WIDTH//2, TUK_HEIGHT//2)
+    character_idle.draw(x, y)
+    update_canvas()
+    handle_events()
+    delay(0.1)
+
+close_canvas()
